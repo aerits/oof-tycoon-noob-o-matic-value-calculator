0e6c7451eaf42e7bfec5fbee5c785413e72d58b1
aerits <stev_nm@protonmail.com>
2022-08-21 23:09:49 +0000

    yep

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # oof-tycoon-noob-o-matic-value-calculator
+
+https://www.roblox.com/games/10325366821/OOF-Tycoon-Update-4
+
+calculates the chance of gaining money through the noob o matic slot machine in oof tycoon
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int
+noobOMatic (int noobprice, int noobs1, int noobs2, int price)
+{
+  int noob = 0;
+  float percent = 0;
+  for (int i = noobs1 - 1; i < noobs2; i++)
+    {
+      noob += noobprice + 18 * i;
+      cout <<"price of "<< i + 1 << " noobs: " << price - noob << endl;
+      if (price - noob > 0)
+	{
+	  percent += 0;
+	  continue;
+	}
+      percent += 1;
+    }
+  cout <<"chance of gaining money: " << percent / ((noobs2 - noobs1) + 1) << endl;
+  return 1;
+}
+
+int
+main ()
+{
+  int noob = 0;
+  cout << "normal" << endl;
+  noobOMatic (810, 1, 5, 2430);
+  cout << "rare" << endl;
+  noobOMatic (846, 2, 10, 5070);
+  cout << "epic" << endl;
+  noobOMatic (846, 3, 25, 12900);
+  return 0;
+}
